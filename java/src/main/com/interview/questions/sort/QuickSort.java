09d606e6f4675818dad54c5a9bbedb5692d2493d
Jack Tan <jacktan165@gmail.com>
2018-09-30 14:02:30 +1300

    Added some more comments to quick sort

--- java/src/main/com/interview/questions/sort/QuickSort.java
+++ java/src/main/com/interview/questions/sort/QuickSort.java
@@ -10,6 +10,26 @@ public class QuickSort implements Sort {
     // 7. Swap pivot with left index position
     // 7. From the pivot current position, split into two sorts, the left side, and the right side excluding the pivot, and repeat
     // 8. Return if left index is greater than or equal to right index as that means there is only one or less element, which by itself is sorted.
+
+    // e.g. 9 12 9 2 17 1 6
+    // Swap 1 with 9
+    // 1 12 9 2 17 9 6
+    // Swap 12 with 2
+    // 1 2 9 12 17 9 6
+    // Swap 6 which is the pivot with left element 9
+    // 1 2 | 6 | 12 17 9 9
+    // on the left side:
+    // 1 is sorted
+    // 2 is sorted
+    // on the right side
+    // Swap pivot element 9 with 12
+    // 9 17 9 12
+    // Swap 9 with 17
+    // 9 9 17 12
+    // Swap pivot element 17 with 12
+    // 9 9 12 17
+    // 9 9 is sorted
+    // 12 17 is sorted
     @Override
     public Double[] sort(Double[] list) {
         return sort(list, 0, list.length - 1);
@@ -22,6 +42,8 @@ public class QuickSort implements Sort {
 
         int originalLeftIndex = leftIndex;
         int pivotIndex = rightIndex;
+        // Initial right index should not be the pivot
+        rightIndex = rightIndex - 1;
 
         while (leftIndex <= rightIndex) {
             if (list[leftIndex] > list[pivotIndex] && list[rightIndex] < list[pivotIndex]) {
