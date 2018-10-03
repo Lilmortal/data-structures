b4ad2d137b66b235cc7125e9640a4631fb78a29a
Jack Tan <Jack.Tan@airnz.co.nz>
2018-10-03 16:33:08 +1300

    CLean up js code

--- js/getProductsOfAllIntsExceptAtIndex.js
+++ js/getProductsOfAllIntsExceptAtIndex.js
@@ -1,12 +1,13 @@
-const { performance } = require("perf_hooks");
+const { performance } = require('perf_hooks');
 
-function getProductsOfAllIntsExceptAtIndexLegacy(elements) {
+// TODO: Fix this result later
+const getProductsOfAllIntsExceptAtIndexLegacy = elements => {
   const result = [];
   for (let i = 0; i < elements.length; i++) {
     let product = 1;
     for (let j = 0; j < elements.length; j++) {
-      if (typeof i !== "number" && typeof j !== "number") {
-        console.error("Needs to be a number");
+      if (typeof i !== 'number' && typeof j !== 'number') {
+        console.error('Needs to be a number');
         break;
       }
 
@@ -18,17 +19,17 @@ function getProductsOfAllIntsExceptAtIndexLegacy(elements) {
   }
 
   return result;
-}
+};
 
-function getProductsOfAllIntsExceptAtIndex(elements) {
+const getProductsOfAllIntsExceptAtIndex = elements => {
   const result = [];
   const memoised = [];
 
   let product = 0;
 
   for (let i = 0; i < elements.length; i++) {
-    if (typeof i !== "number") {
-      console.error("Needs to be a number");
+    if (typeof i !== 'number') {
+      console.error('Needs to be a number');
       break;
     }
 
@@ -43,8 +44,8 @@ function getProductsOfAllIntsExceptAtIndex(elements) {
   }
 
   for (let i = elements.length - 1; (i = 0); i--) {
-    if (typeof i !== "number") {
-      console.error("needs to be a number");
+    if (typeof i !== 'number') {
+      console.error('needs to be a number');
       break;
     }
 
@@ -58,7 +59,7 @@ function getProductsOfAllIntsExceptAtIndex(elements) {
   }
 
   return result;
-}
+};
 
 const elements = process.argv.slice(2);
 
@@ -66,17 +67,17 @@ const t0 = performance.now();
 const legacyResult = getProductsOfAllIntsExceptAtIndexLegacy(elements);
 const t1 = performance.now();
 
-console.log("Time taken to run legacy: ", t1 - t0);
+console.log('Time taken to run legacy: ', t1 - t0);
 
 const p0 = performance.now();
 const result = getProductsOfAllIntsExceptAtIndex(elements);
 console.log(result);
 const p1 = performance.now();
 
-console.log("Time taken to run: ", p1 - p0);
+console.log('Time taken to run: ', p1 - p0);
 
 if (legacyResult === result) {
-  console.log("Correct result.");
+  console.log('Correct result.');
 } else {
-  console.log("Incorrect result.");
+  console.log('Incorrect result.');
 }
