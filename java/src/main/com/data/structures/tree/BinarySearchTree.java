99b3458a77aecd4f4db01f4ec56e5bac6a503769
Jack Tan <jacktan165@gmail.com>
2018-10-12 01:39:09 +1300

    WIP on pretraversal

--- java/src/main/com/data/structures/tree/BinarySearchTree.java
+++ java/src/main/com/data/structures/tree/BinarySearchTree.java
@@ -2,6 +2,8 @@ package com.data.structures.tree;
 
 import com.data.structures.InvalidInputException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class BinarySearchTree extends BaseTree {
@@ -13,12 +15,13 @@ public class BinarySearchTree extends BaseTree {
     @Override
     public void remove(Double value) throws InvalidInputException {
         int pos = getValuePosition(value, 0);
+        // Test 10,5,30,50,35,40,39 and remove 30
         remove(value, pos);
     }
 
     @Override
     public Double get(int pos) {
-        return null;
+        return this.trees[pos];
     }
 
     @Override
@@ -49,6 +52,10 @@ public class BinarySearchTree extends BaseTree {
         if (this.trees[rightChildPos] != null) {
             int mostLeftChildPos = getMostLeftestChild(rightChildPos);
             swap(mostLeftChildPos, pos);
+
+            Double[] test = new Double[100];
+            Double[] subTreeToBeMoved = getPreTraversal(test, mostLeftChildPos);
+
         } else if (this.trees[leftChildPos] != null) {
             swap(leftChildPos, pos);
         }
@@ -77,5 +84,25 @@ public class BinarySearchTree extends BaseTree {
         } else {
             return pos;
         }
+    }
+
+    private Double[] getPreTraversal(Double[] array, int pos) {
+        int leftChildPos = getLeftChildPos(pos);
+        int rightChildPos = getRightChildPos(pos);
+
+        if (this.trees[pos] != null) {
+            array[pos] = this.trees[pos];
+            this.trees[pos] = null;
+        }
+
+        if (this.trees[leftChildPos] != null) {
+            array = getPreTraversal(array, leftChildPos);
+        }
+
+        if (this.trees[rightChildPos] != null) {
+            array = getPreTraversal(array, rightChildPos);
+        }
+
+        return array;
     }
 }
